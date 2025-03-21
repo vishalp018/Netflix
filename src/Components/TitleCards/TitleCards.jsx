9f83e3c135f2a7d2c30009162766333783ac96ae
VISHAL <palvishal111@gmail.com>
2025-03-21 20:32:36 +0530

    Added many sections and created a fully working navbar component

---
+++ src/Components/TitleCards/TitleCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitleCards.css'
+
+const TitleCards = () => {
+  return (
+    <div className='TitleCards'>
+      
+    </div>
+  )
+}
+
+export default TitleCards
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './player.css'
+
+const Player = () => {
+  return (
+    <div className='player'>
+      
+    </div>
+  )
+}
+
+export default Player
